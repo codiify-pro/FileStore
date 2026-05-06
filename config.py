8c814b6b5f83792eff675be8b56a8778483471bf
Codiify <aaryansah954@gmail.com>
2026-05-06 22:50:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = 6497757690
+OWNER_ID = 2021145517
 
 MSG_EFFECT = 5046509860389126442
 
@@ -24,7 +24,7 @@ DB_NAME = "yato"
 
 FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =   # just put channel id dont add ""
+DB_CHANNEL = -1003582815360 # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
@@ -33,7 +33,7 @@ DB_CHANNEL =   # just put channel id dont add ""
 # Auto Delete Timer (seconds)
 AUTO_DEL = 300
 # Admin IDs
-ADMINS = [6497757690, 6103092779]
+ADMINS = [2021145517]
 # Bot Settings
 DISABLE_BTN = True
 PROTECT = True

12b09dd457a45afc1391c2c5b0279326d32474b0
Codiify <aaryansah954@gmail.com>
2026-05-06 23:00:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ WORKERS = 5
 DB_URI = "mongodb+srv://aaryansah954:QgDQRgyD7VUa7Eho@cluster0.wjo9zfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "yato"
 
-FSUBS = [[-1003016571084, False, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+FSUBS = [-1003582815360, False, 10] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
 DB_CHANNEL = -1003582815360 # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
